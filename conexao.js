9b70c44d515abfb4c2aa5c399b474cf2569956c4
Fábio Oliveira <fabiodeoliveirabr@gmail.com>
2024-12-09 18:52:50 -0300

    OK

---
+++ conexao.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: '172.16.142.16',     // Endereço do servidor MySQL
+  user: 'estudante',          // Usuário do MySQL
+  password: 'senai@2024',      // Senha do MySQL
+  database: 'bd_clinica',// Nome do banco de dados
+  port: 3306             // Porta do MySQL somente se necessário)
+});
+
+db.connect((erro) => {
+  if (erro) {
+    console.error('Erro ao conectar ao MySQL:', erro.message);
+    return;
+  }
+  console.log('Conexão com o MySQL estabelecida com sucesso');
+});
+
+module.exports = db;
